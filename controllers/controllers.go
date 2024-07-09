f198a9f522afff83c9a319a75377dc521e692d63
TheRealImaginary <mazennnnnnnnn@gmail.com>
2024-07-09 16:03:08 +0300

    feat: Add SLOs
    - Make 95% of the requests fast
    - Add SLIs/SLOs recording rules for Errors and Latency
    - Add SLIs/SLOs dashboards

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -8,9 +8,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	successChancePercentageThreshold = 90
+	slowChancePercentageThreshold    = 95
+	fastResponseThreshold            = 300
+)
+
 func ExampleHandler(c *gin.Context) {
 	// simulate request taking time
-	time.Sleep(getRandomLatencyMs())
+	latency := getRandomLatencyMs(1, fastResponseThreshold)
+	slowChance := generateRandomInt(1, 100)
+	if slowChance >= slowChancePercentageThreshold {
+		latency = getRandomLatencyMs(fastResponseThreshold+1, 1000)
+	}
+	time.Sleep(latency)
 
 	// simulate a random status code
 	c.JSON(getRandomStatusCode(), gin.H{
@@ -19,20 +30,19 @@ func ExampleHandler(c *gin.Context) {
 }
 
 // getRandomLatencyMs generates a random request latency between 100ms and 1000ms
-func getRandomLatencyMs() time.Duration {
-	return time.Duration(generateRandomInt(100, 1000) * int(time.Millisecond))
+func getRandomLatencyMs(min, max int) time.Duration {
+	return time.Duration(generateRandomInt(min, max) * int(time.Millisecond))
 }
 
 // getRandomStatusCode generates a random status code that's 200 90% of the time
 func getRandomStatusCode() int {
 	codes := []int{
 		http.StatusBadRequest,
-		http.StatusOK,
 		http.StatusGatewayTimeout,
 	}
 
 	chance200 := generateRandomInt(1, 100)
-	if chance200 <= 90 {
+	if chance200 <= successChancePercentageThreshold {
 		return http.StatusOK
 	}
 
